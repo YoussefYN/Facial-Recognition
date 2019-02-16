5fc98db0ea927f912bbd45e7e0c81c8e8434cfeb
Youssef Youssry <youssef.yossry.2014@gmail.com>
2019-02-16 18:28:16 +0300

    Change internal variables names.

--- data_loader.py
+++ data_loader.py
@@ -10,63 +10,63 @@ from pandas import read_csv
 class Loader:
 
     def __init__(self, data_path, path, im_size=299):
-        self.model_path = path
-        self.im_size = im_size
+        self._model_path = path
+        self._img_size = im_size
         self._assemble_cnn_graph()
         self._open_session()
-        self.data_path = data_path
-        self.images_mat = [[os.path.join(x, y) for y in os.listdir(os.path.join(self.data_path, x))]
-                           for x in os.listdir(self.data_path)]
-        self.idxs = [0] * len(self.images_mat)
+        self._data_path = data_path
+        self._images_dirs = [[os.path.join(x, y) for y in os.listdir(os.path.join(self._data_path, x))]
+                             for x in os.listdir(self._data_path)]
+        self._img_idxs = [0] * len(self._images_dirs)
         try:
-            self.cache = pickle.load(open("img_cach_file.pkl", "rb"))
+            self._cache = pickle.load(open("img_cach_file.pkl", "rb"))
             print("Re-read Cache")
         except (OSError, IOError):
-            self.cache = dict()
+            self._cache = dict()
 
     def _assemble_cnn_graph(self):
-        with tf.gfile.GFile(self.model_path, 'rb') as f:
+        with tf.gfile.GFile(self._model_path, 'rb') as f:
             graph_def = tf.GraphDef()
             graph_def.ParseFromString(f.read())
             tf.import_graph_def(graph_def, name='')
-            self.input_ = tf.get_default_graph().get_tensor_by_name("Model_Input:0")
-            self.cnn_embedding_ = tf.get_default_graph().get_tensor_by_name("Model_Output:0")
+            self._input = tf.get_default_graph().get_tensor_by_name("Model_Input:0")
+            self._cnn_embedding = tf.get_default_graph().get_tensor_by_name("Model_Output:0")
 
     def _load_image(self, path):
-        image = cv2.resize(cv2.imread(os.path.join(self.data_path, path)), (self.im_size, self.im_size))
-        embedding = self.sess.run(self.cnn_embedding_, feed_dict={self.input_: [image]})[0]
-        self.cache[path] = embedding
+        image = cv2.resize(cv2.imread(os.path.join(self._data_path, path)), (self._img_size, self._img_size))
+        embedding = self._sess.run(self._cnn_embedding, feed_dict={self._input: [image]})[0]
+        self._cache[path] = embedding
         return embedding
 
     def _load_preprocess_images(self, batch):
-        return [self.cache[p] if p in self.cache else self._load_image(p)
+        return [self._cache[p] if p in self._cache else self._load_image(p)
                 for p in batch]
 
     def _open_session(self):
-        self.sess = tf.Session()
+        self._sess = tf.Session()
 
     def _save_cache(self):
         file = open("img_cach_file.pkl", "wb")
-        pickle.dump(self.cache, file)
+        pickle.dump(self._cache, file)
         file.close()
 
     def get_next_batch(self, minibatch_sz):
-        dirs_len = len(self.images_mat)
+        dirs_len = len(self._images_dirs)
         anc_batch = []
         pos_batch = []
         neg_batch = []
         for i in range(dirs_len):
-            images = self.images_mat[i]
+            images = self._images_dirs[i]
             length = len(images)
             if length < 2:
                 continue
-            anc_batch.append(images[self.idxs[i]])
-            pos_batch.append(images[(self.idxs[i] + 1) % length])
-            self.idxs[i] = (self.idxs[i] + 1) % length
+            anc_batch.append(images[self._img_idxs[i]])
+            pos_batch.append(images[(self._img_idxs[i] + 1) % length])
+            self._img_idxs[i] = (self._img_idxs[i] + 1) % length
             rand_class = i
-            while (len(self.images_mat[rand_class]) == 0) or (rand_class == i):
+            while (len(self._images_dirs[rand_class]) == 0) or (rand_class == i):
                 rand_class = randint(0, dirs_len - 1)
-            neg_batch.append(self.images_mat[rand_class][self.idxs[rand_class]])
+            neg_batch.append(self._images_dirs[rand_class][self._img_idxs[rand_class]])
 
         def divide(x):
             len_x = len(x)
--- siamese_net.py
+++ siamese_net.py
@@ -13,7 +13,7 @@ class SiameseNet:
             self._determinize_model(seed)
         self._assemble_graph(lr)
         self._open_session()
-        self.loader = Loader(data_path, inception_path)
+        self._loader = Loader(data_path, inception_path)
 
     @staticmethod
     def _model(input_):
@@ -25,32 +25,32 @@ class SiameseNet:
         return layer5
 
     def _assemble_graph(self, lr=0.0001):
-        self.input_anc = tf.placeholder(tf.float32, [None, 2048])
-        self.input_pos = tf.placeholder(tf.float32, [None, 2048])
-        self.input_neg = tf.placeholder(tf.float32, [None, 2048])
-        self.alpha = tf.placeholder(tf.float32, ())
+        self._input_anc = tf.placeholder(tf.float32, [None, 2048])
+        self._input_pos = tf.placeholder(tf.float32, [None, 2048])
+        self._input_neg = tf.placeholder(tf.float32, [None, 2048])
+        self._alpha = tf.placeholder(tf.float32, ())
         with tf.variable_scope('siamese', reuse=tf.AUTO_REUSE):
-            output_anc = self._model(self.input_anc)
-            output_pos = self._model(self.input_pos)
-            output_neg = self._model(self.input_neg)
-        self.term1 = tf.square(tf.norm(output_anc - output_pos, axis=1))
-        self.term2 = tf.square(tf.norm(output_anc - output_neg, axis=1))
-        self.loss = tf.reduce_mean(tf.nn.relu(self.term1 + self.alpha - self.term2))
-        self.train_op = tf.train.AdamOptimizer(learning_rate=lr).minimize(self.loss)
+            output_anc = self._model(self._input_anc)
+            output_pos = self._model(self._input_pos)
+            output_neg = self._model(self._input_neg)
+        self._term1 = tf.square(tf.norm(output_anc - output_pos, axis=1))
+        self._term2 = tf.square(tf.norm(output_anc - output_neg, axis=1))
+        self._loss = tf.reduce_mean(tf.nn.relu(self._term1 + self._alpha - self._term2))
+        self._train_op = tf.train.AdamOptimizer(learning_rate=lr).minimize(self._loss)
 
     def train(self, epochs=10, minibatch_size=256, alpha=0.2, thresh=-0.8):
-        self.sess.run(tf.global_variables_initializer())
-        test_anc_batch, test_pos_batch, test_neg_batch = self.loader.get_test_batches()
+        self._sess.run(tf.global_variables_initializer())
+        test_anc_batch, test_pos_batch, test_neg_batch = self._loader.get_test_batches()
 
         for epoch in range(epochs):
-            anc_batches, pos_batches, neg_batches = self.loader.get_next_batch(minibatch_size)
+            anc_batches, pos_batches, neg_batches = self._loader.get_next_batch(minibatch_size)
             for anc_batch, pos_batch, neg_batch \
                     in zip(anc_batches, pos_batches, neg_batches):
-                loss, _ = self.sess.run([self.loss, self.train_op],
-                                        feed_dict={self.input_anc: anc_batch,
-                                                   self.input_pos: pos_batch,
-                                                   self.input_neg: neg_batch,
-                                                   self.alpha: alpha})
+                loss, _ = self._sess.run([self._loss, self._train_op],
+                                         feed_dict={self._input_anc: anc_batch,
+                                                   self._input_pos: pos_batch,
+                                                   self._input_neg: neg_batch,
+                                                   self._alpha: alpha})
 
             accuracy, test_loss = self._evaluate(test_anc_batch,
                                                  test_pos_batch,
@@ -59,21 +59,21 @@ class SiameseNet:
                                                  alpha=alpha)
 
             print("Epoch %d, test acc %.4f, test batch loss %.4f" % (epoch, accuracy, test_loss))
-        self.loader._save_cache()
+        self._loader._save_cache()
 
     def _evaluate(self, anc_batch, pos_batch, neg_batch, thresh, alpha):
-        same, different, loss = self.sess.run([self.term1, self.term2, self.loss],
-                                              feed_dict={self.input_anc: anc_batch,
-                                                         self.input_pos: pos_batch,
-                                                         self.input_neg: neg_batch,
-                                                         self.alpha: alpha})
+        same, different, loss = self._sess.run([self._term1, self._term2, self._loss],
+                                               feed_dict={self._input_anc: anc_batch,
+                                                         self._input_pos: pos_batch,
+                                                         self._input_neg: neg_batch,
+                                                         self._alpha: alpha})
         same_acc = np.where(same <= -1 * thresh, 1, 0)
         different_acc = np.where(different <= -1 * thresh, 0, 1)
         acc = np.append(same_acc, different_acc)
         return acc.mean(), loss
 
     def _open_session(self):
-        self.sess = tf.Session()
+        self._sess = tf.Session()
 
     @staticmethod
     def _determinize_model(seed):
